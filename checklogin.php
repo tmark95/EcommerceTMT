3e9f53b2ed23b109722c71323b5609cc55cf4a08
toiaa <victoria@geddes.com.ar>
2018-09-05 12:51:44 -0300

    toia no presto atencion en mi clase

---
+++ checklogin.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+
+<?php
+
+$host_db = "localhost";
+$user_db = "root";
+$pass_db = "";
+$db_name = "basedatosmaster";
+$tbl_name = "usuarios";
+
+$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+
+if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+$sql = "SELECT * FROM $tbl_name WHERE username = '$username'";
+
+$result = $conexion->query($sql);
+
+
+if ($result->num_rows > 0) {     
+ }
+ $row = $result->fetch_array(MYSQLI_ASSOC);
+ if (password_verify($password, $row['password'])) { 
+ 
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    $_SESSION['start'] = time();
+    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
+
+    echo "<a href=index.html>Home</a>";
+
+ } else { 
+   echo "Username o Password estan incorrectos.";
+
+   echo "<br><a href='login.html'>Volver a Intentarlo</a>";
+ }
+ mysqli_close($conexion); 
+ ?>
